00a181660a2a68fbf3b1c7b2ebb1654ef6f01706
Felws <101949239+Felws@users.noreply.github.com>
2022-05-28 16:47:21 -0300

    before nav change
    version before nav change

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,19 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "../Imagens/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "../Imagens/close_white_36dp.svg";
-    }
-}
+
+
+// const navSlide = () => {
+//     const burger = document.querySelector(".burger");
+//     const nav = document.querySelector(".nav-list");
+//     const nav = document.querySelector(".nav-list li")
+
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle("nav-active");
+
+
+//         navItems.forEach(link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = "";
+//             } else {
+//                 link.style.animation = `navLinkFade 0.5s easy fowards ${index / 7 + 0.3}s`;
+//             }
+//         });
+//     };
